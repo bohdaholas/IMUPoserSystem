87168e9aa635a0ce14db91146d700e5790b4feef
bohdaholas <bohdan.ivashko@ucu.edu.ua>
2024-04-06 03:02:33 +0300

    add nvs_utils to central & peripheral nodes

---
+++ IMUNode/central/main/nvs_utils.h
@@ -0,0 +1,12 @@
+#ifndef NVS_STORE_OPTIONS_NVS_H
+#define NVS_STORE_OPTIONS_NVS_H
+
+#include <stddef.h>
+
+void nvs_init();
+
+void write_string_to_nvs(const char* key, const char* value);
+
+void read_string_from_nvs(const char* key, char* value, size_t* length);
+
+#endif //NVS_STORE_OPTIONS_NVS_H
