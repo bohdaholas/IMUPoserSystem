6ee8b346ca992413555539193f5c07566988c17d
bohdaholas <bohdan.ivashko@ucu.edu.ua>
2024-04-19 00:58:11 +0300

    quaternion based joints orientation

--- Visualizer/Visualizer.py
+++ Visualizer/Visualizer.py
@@ -1,36 +1,55 @@
 import bge
-from collections import OrderedDict
-import socket
-from random import randint
-import threading
 import bpy
-from mathutils import *
-from math import *
-import time
+from mathutils import Quaternion
+import socket
+import threading
+import math
+from enum import Enum
+class Axis(Enum):
+    X = 1
+    Y = 2
+    Z = 3
 
-HOST = 'localhost'
-PORT = 1234
-BUFFER_SIZE = 1024
-ax = ay = az = 0
+class Human:
+    def __init__(self):
+        self.bones = bpy.data.objects['SMPLX-male'].pose.bones
+        self.joints_quaternion_orientation = {
+            'pelvis': (1, 0, 0, 0),
+            'spine1': (1, 0, 0, 0),
+            'head': (1, 0, 0, 0),
+            'left_hip': (1, 0, 0, 0),
+            'right_hip': (1, 0, 0, 0),
+            'left_knee': (1, 0, 0, 0),
+            'right_knee': (1, 0, 0, 0),
+            'left_shoulder': (1, 0, 0, 0),
+            'right_shoulder': (1, 0, 0, 0),
+            'left_elbow': (1, 0, 0, 0),
+            'right_elbow': (1, 0, 0, 0)
+        }
+    def apply_quaternion_rotation(self, joint: str, orientation: tuple):
+        self.bones[joint].rotation_quaternion = Quaternion(orientation)
 
-bones = bpy.data.objects['SMPLX-male'].pose.bones
+    def apply_around_axis_quaternion_rotation(self, joint:str, axis: Axis, angle: int):
+        current_orientation = self.joints_quaternion_orientation[joint]
+        q = Quaternion(current_orientation)
+        axis_tuple = (0, 0, 0)
+        match axis:
+            case Axis.X:
+                axis_tuple = (1, 0, 0)
+            case Axis.Y:
+                axis_tuple = (0, 1, 0)
+            case Axis.Z:
+                axis_tuple = (0, 0, 1)
+        q_axis_rotation = Quaternion(axis_tuple, math.radians(angle))
+        q @= q_axis_rotation
+        self.bones[joint].rotation_quaternion = q
+        self.joints_quaternion_orientation[joint] = (q.w, q.x, q.y, q.z)
 
-pose = {
-    'pelvis': (0, 0, 0),
-    'spine1': (0, 0, 0), 
-    'head': (0, 0, 0), 
-    'left_hip': (0, 0, 0), 
-    'right_hip': (0, 0, 0), 
-    'left_knee': (0, 0, 0), 
-    'right_knee': (0, 0, 0), 
-    'left_shoulder': (0, 0, 0), 
-    'right_shoulder': (0, 0, 0), 
-    'left_elbow': (0, 0, 0), 
-    'right_elbow': (0, 0, 0)
-}
+human = Human()
 
 def socket_listener():
-    global ax, ay, az
+    PORT = 1234
+    BUFFER_SIZE = 1024
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
         server_socket.bind(('', PORT))
@@ -39,34 +58,21 @@ def socket_listener():
         while True:
             data, addr = server_socket.recvfrom(BUFFER_SIZE)
             data = data.decode().split('\n')[-2].split(',')
-            body_segment, *orientation = data
-            ax, ay, az = [float(angle) for angle in orientation]
-            if body_segment not in pose:
-                print("such body segment either doesn't exist or it's not supported")
-                continue
-            pose[body_segment] = (ax, ay, az)
-
-        
-def rotate_body_segment(body_segment, orientation):
-    if body_segment not in pose:
-        print("such body segment either doesn't exist or it's not supported")
-        return
-    orientation_radians = tuple(radians(angle) for angle in orientation_degrees)
-    bones[body_segment].rotation_euler = Euler(orientation_radians, 'XYZ')
-    print(f"{body_segment} orient {orientation_radians}")
-    print(f"Orientation: {orientation_radians}, Body Segment: {body_segment}")
+            joint, *orientation = data
+            w, rx, ry, rz = [float(angle) for angle in orientation]
+            if human.joints_quaternion_orientation[joint] != (w, ry, rz, rx):
+                print(f"Orientation: {orientation}, Body Segment: {joint}")
+                human.joints_quaternion_orientation[joint] = (w, ry, rz, rx)
 
 
 class HumanVisualizer(bge.types.KX_PythonComponent):
-    args = OrderedDict([
-    ])
-
     def start(self, args):
-        for body_segment in pose.keys():  
-            bones[body_segment].rotation_mode = 'XYZ'
+        for joint in human.joints_quaternion_orientation.keys():
+            human.bones[joint].rotation_mode = 'QUATERNION'
         thread = threading.Thread(target=socket_listener, daemon=True)
-        thread.start()
-        
+        thread.start() 
+
     def update(self):
-        for body_segment, orientation in pose.items():  
-            rotate_body_segment(body_segment, orientation)
+        for joint, orientation in human.joints_quaternion_orientation.items():
+            human.apply_quaternion_rotation(joint, orientation)
+
